c0117bdf59b9507a9272c66ff4999b04601b52b8
Anton Shabinskiy <a.shabinskiy@outlook.com>
2018-08-16 20:16:04 +0300

    Prepare the demo project

---
+++ HelloCsharp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace KmaOoad18.Assignments.Demo
+{
+    public class HelloCsharp
+    {
+        private const int CsharpBirthYear = 2000;
+
+        public int GetCsharpAge()
+        {
+            // Replace with your implementation
+            throw new NotImplementedException();
+        }
+
+        private int GetAge(int currentYear, int birthYear)
+        {
+            // Replace with your implementation
+            throw new NotImplementedException();
+        }
+
+        public virtual int GetCurrentYear()
+        {
+            // Replace with your implementation
+            throw new NotImplementedException();
+        }
+
+        public static void Main(string[] args)
+        {
+            // Get C# age here and print it to console
+        }
+    }
+}
---
+++ _tests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NSubstitute;
+using Xunit;
+
+namespace KmaOoad18.Assignments.Demo
+{
+    // DON'T MODIFY THIS FILE
+    public class _tests
+    {
+        [Theory]
+        [InlineData(2018, 18)]
+        [InlineData(2021, 21)]
+        [InlineData(1990, 0)]
+        public void CanCalculateCsharpAge(int supposedYear, int assertedAge)
+        {
+            // Given
+            var s = Substitute.ForPartsOf<HelloCsharp>();
+
+            s.GetCurrentYear().Returns(supposedYear);
+
+            // When
+            var age = s.GetCsharpAge();
+
+            // Then
+            Assert.Equal(assertedAge, age);
+        }
+
+        [Fact]
+        public void CanGetCurrentYear()
+        {
+            // Given
+            var hw = new HelloCsharp();
+
+            // When
+            var y = hw.GetCurrentYear();
+
+            // Then
+            Assert.Equal(y, DateTime.Now.Year);
+        }
+    }
+}
